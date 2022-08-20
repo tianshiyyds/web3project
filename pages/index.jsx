d25e4df7402993e2bf10c29a7811bbde47b7f3cd
tianshiyyds <111618713+tianshiyyds@users.noreply.github.com>
2022-08-20 11:20:49 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = "JOPMtfVTuzJFdezYtt69y3pVPknp5cBn"
+    const api_key = "JOPMtfVTuzJFdezYtt69y3pVPknp5cB"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
